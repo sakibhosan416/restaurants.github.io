e2ed9ebaa0b6ba121de2bc181b8092d0fed22501
sakibhosan416 <67134448+sakibhosan416@users.noreply.github.com>
2020-09-05 01:00:06 +0600

    restaurants website

---
+++ js/main.js
@@ -0,0 +1,78 @@
+
+		  
+
+// fillter pluging
+	var mixer = mixitup('.gallery');
+
+
+// counter
+		$('.count').counterUp({
+		    delay:2 ,
+		    time: 1000
+			});
+
+// button popup
+	function popUp(){
+		swal("submitted successfully!", "I received your email!", "success");
+
+	}
+// animation
+
+	new WOW().init();
+
+// scrolling top	
+	$('.top').click(function(){
+		$("html, body").animate({
+			scrollTop:0
+
+		},1000);
+	});
+	$(window).scroll(function(){
+		var ourwindow = $(this).scrollTop();
+		if(ourwindow<800){
+			$('.top').fadeOut();
+		}else{
+			$('.top').fadeIn();
+		}
+	});
+
+
+// scrolling menu
+
+	$('.navbar-nav a[href^="#"]').click(function(e) {
+		e.preventDefault();
+		var target = this.hash;
+		$('html, body').animate({
+			scrollTop: $(target).offset().top -50
+		},500);
+	});
+
+	$('.up-down a[href^="#"]').click(function(e) {
+		e.preventDefault();
+		var target = this.hash;
+		$('html, body').animate({
+			scrollTop: $(target).offset().top -50
+		},500);
+	});
+
+	$('.about-content a[href^="#"]').click(function(e) {
+		e.preventDefault();
+		var target = this.hash;
+		$('html, body').animate({
+			scrollTop: $(target).offset().top -50
+		},500);
+	});
+
+	$('.btn a[href^="#"]').click(function(e) {
+		e.preventDefault();
+		var target = this.hash;
+		$('html, body').animate({
+			scrollTop: $(target).offset().top -50
+		},500);
+	});
+
+// for change menu color
+
+	$('.navbar-nav li').click(function(){
+		$(this).addClass('active').siblings().removeClass('active');
+	});
